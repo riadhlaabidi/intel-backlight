37ef4bfa11bd96f7cb6465ebe937e1fc27e31e61
Riadh Laabidi <riadh.by.laabidi@gmail.com>
2024-09-14 19:09:20 +0100

    Add todos

--- setup.sh
+++ setup.sh
@@ -6,4 +6,8 @@ cp -i $script_dir/backlight.rules -t /etc/udev/rules.d
 
 echo "Copied backlight udev rule"
 
+# TODO: Install intel-backlight bin to /usr/local/bin
+# TODO: Install brightness bin to /usr/local/bin
+# TODO: Write steps in README.md
+
 exit 0
